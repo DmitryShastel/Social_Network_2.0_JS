7e0cf247d6a53638abe2f4b43622db805e80ab24
DmitryShastel <dmshastel@gmail.com>
2022-09-29 21:14:33 +0300

    The Header is created

--- social_network_2.0_js/src/App.js
+++ social_network_2.0_js/src/App.js
@@ -7,7 +7,7 @@ function App() {
         <div className='app-wrapper'>
 
             <header className='header'>
-
+                <img src='https://st3.depositphotos.com/1764439/17897/i/600/depositphotos_178973448-stock-photo-yin-yang-tree-logo.jpg'/>
             </header>
 
 
