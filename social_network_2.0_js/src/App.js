49129d7028dcf9ab2aa6016bf60d8ed49aa36ce2
DmitryShastel <dmshastel@gmail.com>
2022-09-29 21:38:43 +0300

    The Content is created

--- social_network_2.0_js/src/App.js
+++ social_network_2.0_js/src/App.js
@@ -31,7 +31,14 @@ function App() {
             </nav>
 
             <div className='content'>
-
+                <img src='https://www.planetware.com/wpimages/2022/01/world-most-beautiful-islands-st-lucia-caribbean.jpg'/>
+                <div className='postsContent'>
+                    <div>Ava + description</div>
+                    <div>My posts</div>
+                    <div>New post</div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
 
             </div>
 
