9f572da21594d1777043a21cfd657a930810b139
DmitryShastel <dmshastel@gmail.com>
2022-09-30 20:30:50 +0300

    The social network 1-12 header.module.css is created

--- social_network_2.0_js/src/components/Header/Header.jsx
+++ social_network_2.0_js/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css'
 
 export const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://st3.depositphotos.com/1764439/17897/i/600/depositphotos_178973448-stock-photo-yin-yang-tree-logo.jpg'/>
         </header>
     )
