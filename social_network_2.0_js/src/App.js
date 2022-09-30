3f69e109462a97610ed9dc683bafdda100709651
DmitryShastel <dmshastel@gmail.com>
2022-09-30 18:59:57 +0300

    The social network 1-12 is done

--- social_network_2.0_js/src/App.js
+++ social_network_2.0_js/src/App.js
@@ -1,55 +1,18 @@
 import './App.css';
 import React from "react";
+import {Header} from "./components/Header/Header";
+import {Navbar} from "./components/Navbar/Navbar";
+import {Profile} from "./components/Profile/Profile";
+import {Footer} from "./components/Footer/Footer";
 
 
 function App() {
     return (
         <div className='app-wrapper'>
-
-            <header className='header'>
-                <img src='https://st3.depositphotos.com/1764439/17897/i/600/depositphotos_178973448-stock-photo-yin-yang-tree-logo.jpg'/>
-            </header>
-
-
-            <nav className='navbar'>
-                <div className='navContent'>
-                    <div>
-                        <a href=''>Profile</a>
-                    </div>
-                    <div>
-                        <a href=''>Message</a>
-                    </div>
-                    <div>
-                        <a href=''>News</a>
-                    </div>
-                    <div>
-                        <a href=''>Music</a>
-                    </div>
-                    <div>
-                        <a href=''>Settings</a>
-                    </div>
-                </div>
-
-
-            </nav>
-
-            <div className='content'>
-                <img src='https://www.planetware.com/wpimages/2022/01/world-most-beautiful-islands-st-lucia-caribbean.jpg'/>
-                <div className='postsContent'>
-                    <div>Ava + description</div>
-                    <div>My posts</div>
-                    <div>New post</div>
-                    <div>post 1</div>
-                    <div>post 2</div>
-                </div>
-
-            </div>
-
-            <div className='footer'>
-
-            </div>
-
-
+            <Header/>
+            <Navbar/>
+            <Profile/>
+            <Footer/>
         </div>
     )
 }
---
+++ social_network_2.0_js/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://st3.depositphotos.com/1764439/17897/i/600/depositphotos_178973448-stock-photo-yin-yang-tree-logo.jpg'/>
+        </header>
+    )
+}
