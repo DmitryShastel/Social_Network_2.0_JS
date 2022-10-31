23bd50f44261496a67fdb340fcfd0a9e1ed96b35
DmitryShastel <dmshastel@gmail.com>
2022-10-31 21:53:45 +0300

    The social network 1-19 is done

--- social_network_2.0_js/src/App.js
+++ social_network_2.0_js/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import React from "react";
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
-import {Footer} from "./components/Footer/Footer";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Profile} from "./components/Profile/Profile";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 
 function App() {
@@ -19,9 +20,12 @@ function App() {
                     <Routes>
                         <Route path='/profile' element={<Profile/>}/>
                         <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/settings' element={<Settings/>}/>
                     </Routes>
                 </div>
-               
+
                 {/*<Footer/>*/}
             </div>
         </BrowserRouter>
