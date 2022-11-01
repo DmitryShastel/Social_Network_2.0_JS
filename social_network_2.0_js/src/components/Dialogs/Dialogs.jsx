80b6584ffa8dade6d13f612c53a3e421a01b6a37
DmitryShastel <dmshastel@gmail.com>
2022-11-01 08:35:20 +0300

    The social network 1-21 Dialogs is created

--- social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
+++ social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,25 @@
 import React from "react";
-
+import s from'./Dialogs.module.css'
 
 export const Dialogs = (props) => {
     return (
-        <div >
-            Dialogs
+        <div className={s.dialogs} >
+          <div className={s.dialogsItem}>
+              <div className={s.dialog}>
+                  Dima
+              </div>
+              <div className={s.dialog}>
+                  Sacha
+              </div>
+              <div className={s.dialog}>
+                  Nikolas
+              </div>
+          </div>
+            <div className={s.message}>
+                <div>hi</div>
+                <div>buy</div>
+                <div>how are you</div>
+            </div>
         </div>
     )
 }
