cfc26b5f663c4be52b0e0923a2e0f39b09b3103e
DmitryShastel <dmshastel@gmail.com>
2022-11-01 10:26:16 +0300

    The social network 1-23 the component DialogItem is changed

--- social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
+++ social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,10 @@ import {NavLink} from "react-router-dom";
 
 
 const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id
+
     return (
-        <NavLink to='/dialogs/1' className={navData => navData.isActive ? s.active : s.item}>
+        <NavLink to={path} className={navData => navData.isActive ? s.active : s.item}>
             {props.name}
         </NavLink>
     )
@@ -18,23 +20,14 @@ export const Dialogs = (props) => {
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
 
-                <div className={`${s.dialog} ${s.active}`}>
-                    <DialogItem name='Dima' id='1'/>
-                </div>
 
+                <DialogItem name='Dima' id='1'/>
+                <DialogItem name='Sacha' id='2'/>
+                <DialogItem name='Nikolas' id='3'/>
 
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/2'>
-                        Sacha
-                    </NavLink>
-                </div>
-                <div className={s.dialog}>
-                    <NavLink to='/dialogs/3'>
-                        Nikolas
-                    </NavLink>
-                </div>
 
             </div>
+
             <div className={s.message}>
                 <div>hi</div>
                 <div>buy</div>
