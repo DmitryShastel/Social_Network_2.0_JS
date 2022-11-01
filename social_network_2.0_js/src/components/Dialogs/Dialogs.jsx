b0a8e7ea220da3f01bf2bdec14f5f9c3019629dc
DmitryShastel <dmshastel@gmail.com>
2022-11-01 10:33:05 +0300

    The social network 1-23 the component DialogItem is changed2

--- social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
+++ social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
@@ -19,13 +19,9 @@ export const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-
-
                 <DialogItem name='Dima' id='1'/>
                 <DialogItem name='Sacha' id='2'/>
                 <DialogItem name='Nikolas' id='3'/>
-
-
             </div>
 
             <div className={s.message}>
