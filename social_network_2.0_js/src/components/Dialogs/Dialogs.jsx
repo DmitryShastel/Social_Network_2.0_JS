95fc738ee68c0e1b76c37947b794a1f1e3985ff7
DmitryShastel <dmshastel@gmail.com>
2022-11-01 10:10:07 +0300

    The social network 1-23 the component DialogItem is created

--- social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
+++ social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
+
+const DialogItem = (props) => {
+    return (
+        <NavLink to='/dialogs/1' className={navData => navData.isActive ? s.active : s.item}>
+            {props.name}
+        </NavLink>
+    )
+
+}
+
+
 export const Dialogs = (props) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
+
                 <div className={`${s.dialog} ${s.active}`}>
-                    <NavLink to='/dialogs/1' className={ navData => navData.isActive ? s.active : s.item}>
-                        Dima
-                    </NavLink>
+                    <DialogItem name='Dima' id='1'/>
                 </div>
+
+
                 <div className={s.dialog}>
                     <NavLink to='/dialogs/2'>
                         Sacha
