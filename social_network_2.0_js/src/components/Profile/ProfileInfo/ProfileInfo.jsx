9b7d580aec057694c436a34d32583e2959747810
DmitryShastel <dmshastel@gmail.com>
2022-11-18 14:28:29 +0300

    The component OnClick is created

--- social_network_2.0_js/src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ social_network_2.0_js/src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -7,8 +7,6 @@ export const ProfileInfo = () => {
         <div>
             <div>
                 <img src='https://www.planetware.com/wpimages/2022/01/world-most-beautiful-islands-st-lucia-caribbean.jpg'/>
-                <div className={s.postsContent}>
-                </div>
             </div>
 
         </div>
