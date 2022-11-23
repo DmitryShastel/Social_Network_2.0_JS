4c629ded0c97a15fcf5bd30230c43ff8513cf915
DmitryShastel <dmshastel@gmail.com>
2022-11-23 09:14:01 +0300

    The social network 1-25 is done

--- social_network_2.0_js/src/components/Profile/MyPosts/MyPosts.jsx
+++ social_network_2.0_js/src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,25 +4,25 @@ import s from './MyPosts.module.css'
 
 export const MyPosts = () => {
 
-    let postData = [
-        {id: 1, message:'Hi how are you', like: 15},
-        {id: 2, message:'It is my first post', like: 23}
+    let posts = [
+        {id: 1, message: 'Hi how are you', like: 15},
+        {id: 2, message: 'It is my first post', like: 23}
     ]
 
+    let postElements = posts.map(p => <Post id={p.id} message={p.message} like={p.like}/>)
+
     return (
-           <div className={s.posts}>
-               <div>Ava + description</div>
-               <h3>My posts</h3>
-               <div>
-                    <textarea></textarea>
-                   <div className={s.button}>
-                       <button>Add Post</button>
-                   </div>
+        <div className={s.posts}>
+            <div>Ava + description</div>
+            <h3>My posts</h3>
+            <div>
+                <textarea></textarea>
+                <div className={s.button}>
+                    <button>Add Post</button>
+                </div>
 
-               </div>
-
-               <Post id={postData[0].id} message={postData[0].message} like={postData[0].like}/>
-               <Post id={postData[1].id} message={postData[1].message} like={postData[1].like}/>
-           </div>
+            </div>
+            {postElements}
+        </div>
     )
 }
