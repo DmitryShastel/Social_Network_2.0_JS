de45f780f1b0ba7e69b1ead66ec941d9a3d969e5
DmitryShastel <dmshastel@gmail.com>
2022-11-24 00:28:42 +0300

    The social network 1-26 the DialodItem is created

---
+++ social_network_2.0_js/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from './../Dialogs.module.css'
+import {NavLink} from "react-router-dom";
+
+
+const DialogItem = (props) => {
+    let path = '/dialogs/' + props.id
+
+    return (
+        <NavLink to={path} className={navData => navData.isActive ? s.active : s.item}>
+            {props.name}
+        </NavLink>
+    )
+
+}
