e4b8eda12cf53ddbe4167894d7af4f0818a25dee
DmitryShastel <dmshastel@gmail.com>
2022-11-23 09:02:11 +0300

    The social network 1-25 the dialogsElements is creates

--- social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
+++ social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
@@ -15,38 +15,38 @@ const DialogItem = (props) => {
 }
 
 const Message = (props) => {
-    return(
-        <div >{props.message}</div>
+    return (
+        <div>{props.message}</div>
     )
 }
 
 
 export const Dialogs = (props) => {
 
-    let dialogData = [
+    let dialogs = [
         {id: 1, name: 'Dima'},
         {id: 2, name: 'Sacha'},
         {id: 3, name: 'Nikolas'}
     ]
 
-    let messageData = [
+    let message = [
         {id: 1, message: 'hi'},
         {id: 2, message: 'buy'},
         {id: 3, message: 'buy now'}
     ]
 
+    let dialogElements = dialogs.map(m => <DialogItem name={m.name} id={m.id}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
-                <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
-                <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
+                {dialogElements}
             </div>
 
             <div className={s.message}>
-                <Message message={messageData[0].message} id={messageData[0].id}/>
-                <Message message={messageData[1].message} id={messageData[1].id}/>
-                <Message message={messageData[2].message} id={messageData[2].id}/>
+                {/*<Message message={messageData[0].message} id={messageData[0].id}/>*/}
+                {/*<Message message={messageData[1].message} id={messageData[1].id}/>*/}
+                {/*<Message message={messageData[2].message} id={messageData[2].id}/>*/}
             </div>
         </div>
     )
