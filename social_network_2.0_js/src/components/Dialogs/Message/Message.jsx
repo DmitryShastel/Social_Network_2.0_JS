4270f8a3f830fc87b9e98e90f3d1bab2be88dca8
DmitryShastel <dmshastel@gmail.com>
2022-11-24 00:30:09 +0300

    The social network 1-26 the Message is created

--- social_network_2.0_js/src/components/Dialogs/Message/Message.jsx
+++ social_network_2.0_js/src/components/Dialogs/Message/Message.jsx
@@ -1,52 +1,8 @@
 import React from "react";
-import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
 
-
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id
-
-    return (
-        <NavLink to={path} className={navData => navData.isActive ? s.active : s.item}>
-            {props.name}
-        </NavLink>
-    )
-
-}
 
 const Message = (props) => {
     return (
         <div>{props.message}</div>
-    )
-}
-
-
-export const Message = (props) => {
-
-    let dialogs = [
-        {id: 1, name: 'Dima'},
-        {id: 2, name: 'Sacha'},
-        {id: 3, name: 'Nikolas'}
-    ]
-
-    let message = [
-        {id: 1, message: 'hi'},
-        {id: 2, message: 'buy'},
-        {id: 3, message: 'buy now'}
-    ]
-
-    let dialogElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
-    let messageElements = message.map(m => <Message message={m.message} id={m.id}/>)
-
-    return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogElements}
-            </div>
-
-            <div className={s.message}>
-                {messageElements}
-            </div>
-        </div>
     )
 }
