e43c846cd7ec2f3b7edd136ce147f6fb8d96dd98
DmitryShastel <dmshastel@gmail.com>
2022-11-28 19:12:32 +0300

    The social network 1-26 the MyPosts is changed

--- social_network_2.0_js/src/components/Profile/MyPosts/MyPosts.jsx
+++ social_network_2.0_js/src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,14 +2,9 @@ import React from "react";
 import {Post} from "./MyPost/Post";
 import s from './MyPosts.module.css'
 
-export const MyPosts = () => {
+export const MyPosts = (props) => {
 
-    let posts = [
-        {id: 1, message: 'Hi how are you', like: 15},
-        {id: 2, message: 'It is my first post', like: 23}
-    ]
-
-    let postElements = posts.map(p => <Post id={p.id} message={p.message} like={p.like}/>)
+    let postElements = props.posts.map(p => <Post id={p.id} message={p.message} like={p.like}/>)
 
     return (
         <div className={s.posts}>
--- social_network_2.0_js/src/components/Profile/Profile.jsx
+++ social_network_2.0_js/src/components/Profile/Profile.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import s from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
+
+
 export const Profile = () => {
+
+    let posts = [
+        {id: 1, message: 'Hi how are you', like: 15},
+        {id: 2, message: 'It is my first post', like: 23}
+    ]
+
+
     return (
         <div>
             <div>
                 <ProfileInfo/>
-                <MyPosts/>
+                <MyPosts posts={posts}/>
             </div>
         </div>
     )
