201ef07cad571e7be576456d767d87c9607343e1
DmitryShastel <dmshastel@gmail.com>
2022-11-29 21:12:40 +0300

    The social network 1-29 is done

--- social_network_2.0_js/src/components/Dialogs/Message/Message.jsx
+++ social_network_2.0_js/src/components/Dialogs/Message/Message.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import s from './../Dialogs.module.css'
 
 export const Message = (props) => {
     return (
-        <div>{props.message}</div>
+        <div>
+            <img className={s.imgMessage} src='https://upload.wikimedia.org/wikipedia/commons/0/05/Google_Messages_logo.svg'/>
+            {props.message}</div>
     )
 }
