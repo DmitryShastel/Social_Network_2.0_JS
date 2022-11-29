13aa5560610972f1c430d91526ba9929f2dd7263
DmitryShastel <dmshastel@gmail.com>
2022-11-29 20:56:01 +0300

    The social network 1-29 the Dialogs.css is changed

--- social_network_2.0_js/src/components/Dialogs/DialogItem/DialogItem.jsx
+++ social_network_2.0_js/src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -8,6 +8,7 @@ export const DialogItem = (props) => {
 
     return (
         <NavLink to={path} className={navData => navData.isActive ? s.active : s.item}>
+            <img className={s.image} src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAHXPluq6GtTRPDIHRv5kJPy86uFjp5sO7hg&usqp=CAU'/>
             {props.name}
         </NavLink>
     )
