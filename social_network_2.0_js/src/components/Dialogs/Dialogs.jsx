16a79c5a5c95d0b1c3e3a1bbf35823d069cdf215
DmitryShastel <dmshastel@gmail.com>
2023-01-14 09:21:46 +0300

    The social network 1-31 is done

--- social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
+++ social_network_2.0_js/src/components/Dialogs/Dialogs.jsx
@@ -9,6 +9,13 @@ export const Dialogs = (props) => {
     let dialogElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
     let messageElements = props.state.message.map(m => <Message message={m.message} id={m.id}/>)
 
+    let newMessageElement = React.createRef()
+
+    let addMessage = () => {
+        let message = newMessageElement.current.value
+        alert(message)
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -19,8 +26,8 @@ export const Dialogs = (props) => {
                 {messageElements}
             </div>
             <div>
-                <textarea></textarea>
-                <button onClick={() => {alert('hello')}}>Add Message</button>
+                <textarea ref={newMessageElement}></textarea>
+                <button onClick={addMessage}>Add Message</button>
             </div>
 
         </div>
