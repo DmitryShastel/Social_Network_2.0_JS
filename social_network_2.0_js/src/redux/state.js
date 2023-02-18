5a68b7b14f84ba5a19627f172613d8f0a9349b7b
DmitryShastel <dmshastel@gmail.com>
2023-02-18 18:09:42 +0300

    The social network 1-37 the store all data is fixed

--- social_network_2.0_js/src/redux/state.js
+++ social_network_2.0_js/src/redux/state.js
@@ -27,55 +27,55 @@ export let store = {
     _callSubscribe() {
     },
     addPost() {
-        let newPost = {id: 5, message: state.profilePage.newPostText, like: 0}
-        state.profilePage.posts.push(newPost)
-        state.profilePage.newPostText = ''
-        renderEntireTree(state)
+        let newPost = {id: 5, message: this._state.profilePage.newPostText, like: 0}
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this._callSubscribe(this._state)
     },
     updateNewPost(newText) {
-        state.profilePage.newPostText = newText
-        renderEntireTree(state)
+        this.state.profilePage.newPostText = newText
+        this._callSubscribe(this._state)
     },
     subscriber(observer) {
-        renderEntireTree = observer
+        this._callSubscribe = observer
     }
 }
 
 
-let renderEntireTree = () => {
-}
-export let state = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi how are you', like: 15},
-            {id: 2, message: 'It is my first post', like: 23}
-        ],
-        newPostText: ''
-    },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dima'},
-            {id: 2, name: 'Sacha'},
-            {id: 3, name: 'Nikolas'}
-        ],
-
-        message: [
-            {id: 1, message: 'hi'},
-            {id: 2, message: 'buy'},
-            {id: 3, message: 'buy now'}
-        ]
-    }
-}
-export const addPost = () => {
-    let newPost = {id: 5, message: state.profilePage.newPostText, like: 0}
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    renderEntireTree(state)
-}
-export const updateNewPost = (newText) => {
-    state.profilePage.newPostText = newText
-    renderEntireTree(state)
-}
-export const subscriber = (observer) => {
-    renderEntireTree = observer
-}
+// let renderEntireTree = () => {
+// }
+// export let state = {
+//     profilePage: {
+//         posts: [
+//             {id: 1, message: 'Hi how are you', like: 15},
+//             {id: 2, message: 'It is my first post', like: 23}
+//         ],
+//         newPostText: ''
+//     },
+//     dialogsPage: {
+//         dialogs: [
+//             {id: 1, name: 'Dima'},
+//             {id: 2, name: 'Sacha'},
+//             {id: 3, name: 'Nikolas'}
+//         ],
+//
+//         message: [
+//             {id: 1, message: 'hi'},
+//             {id: 2, message: 'buy'},
+//             {id: 3, message: 'buy now'}
+//         ]
+//     }
+// }
+// export const addPost = () => {
+//     let newPost = {id: 5, message: state.profilePage.newPostText, like: 0}
+//     state.profilePage.posts.push(newPost)
+//     state.profilePage.newPostText = ''
+//     renderEntireTree(state)
+// }
+// export const updateNewPost = (newText) => {
+//     state.profilePage.newPostText = newText
+//     renderEntireTree(state)
+// }
+// export const subscriber = (observer) => {
+//     renderEntireTree = observer
+// }
