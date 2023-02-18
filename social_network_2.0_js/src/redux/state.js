a9e8a9278fb2cd4ce294c2718bb4159528b11ded
DmitryShastel <dmshastel@gmail.com>
2023-02-18 17:54:57 +0300

    The social network 1-37 the store is created

--- social_network_2.0_js/src/redux/state.js
+++ social_network_2.0_js/src/redux/state.js
@@ -1,3 +1,29 @@
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi how are you', like: 15},
+                {id: 2, message: 'It is my first post', like: 23}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dima'},
+                {id: 2, name: 'Sacha'},
+                {id: 3, name: 'Nikolas'}
+            ],
+
+            message: [
+                {id: 1, message: 'hi'},
+                {id: 2, message: 'buy'},
+                {id: 3, message: 'buy now'}
+            ]
+        }
+    },
+    renderEntireTree() {},
+}
+
 
 let renderEntireTree = () => {}
 
