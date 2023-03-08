14c16c2442e2ff4869538540ad5c0b9e9552725f
DmitryShastel <dmshastel@gmail.com>
2023-03-08 16:47:46 +0300

    The social network 1-43 DialogsContainer is changed

--- social_network_2.0_js/src/components/Dialogs/DialogsContainer.jsx
+++ social_network_2.0_js/src/components/Dialogs/DialogsContainer.jsx
@@ -3,47 +3,27 @@ import s from './Dialogs.module.css'
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {sendMessageActionCreator, updateNewMessageBodyActionCreator} from "../../redux/dialogs-reducer";
+import {Dialogs} from "./Dialogs";
 
 
 export const DialogsContainer = (props) => {
 
-    let state = props.store.getState().dialogsPage
-    let dialogElements = state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
-    let messageElements = state.message.map(m => <Message message={m.message} id={m.id}/>)
-    let newMessageBody = state.newMessageBody
-
-
-    let onSendMessageClick = (e) => {
+    //let state = props.store.getState().dialogsPage
+    
+    let onSendMessageClick = () => {
         props.store.dispatch(sendMessageActionCreator())
     }
 
-    let onNewMessageChange = (e) => {
-        let body = e.target.value
+    let onNewMessageChange = (body) => {
+        //let body = e.target.value
         props.store.dispatch(updateNewMessageBodyActionCreator(body))
     }
 
     return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogElements}
-            </div>
-
-            <div className={s.messages}>
-                {messageElements}
-                <div>
-                    
-                    <div><textarea
-                        value={newMessageBody}
-                        onChange={onNewMessageChange}
-                        placeholder='Enter your message'
-                    /></div>
-                    <div>
-                        <button onClick={onSendMessageClick}>Add Message</button>
-                    </div>
-
-                </div>
-            </div>
-        </div>
+        <Dialogs
+            onSendMessageClick={onSendMessageClick}
+            onNewMessageChange={onNewMessageChange}
+        />
     )
 }
 
