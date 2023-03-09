d628b5f625995b12e7be1d775df28dc2904ceaa0
DmitryShastel <dmshastel@gmail.com>
2023-03-09 17:16:53 +0300

    The social network 1-44 DialogsContainer is changed

--- social_network_2.0_js/src/components/Dialogs/DialogsContainer.jsx
+++ social_network_2.0_js/src/components/Dialogs/DialogsContainer.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import {sendMessageActionCreator, updateNewMessageBodyActionCreator} from "../../redux/dialogs-reducer";
 import {Dialogs} from "./Dialogs";
+import {StoreContext} from "../StoreContext";
 
 
 export const DialogsContainer = (props) => {
 
-    let state = props.store.getState().dialogsPage
 
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageActionCreator())
-    }
+    return <StoreContext.Consumer>{
 
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyActionCreator(body))
-    }
+        (store) => {
 
-    return (
-        <Dialogs
-            updateNewMessageBody={onNewMessageChange}
-            sendMessage={onSendMessageClick}
-            dialogsPage={state}
-        />
-    )
+            let state = props.store.getState().dialogsPage
+
+            let onSendMessageClick = () => {
+                props.store.dispatch(sendMessageActionCreator())
+            }
+
+            let onNewMessageChange = (body) => {
+                props.store.dispatch(updateNewMessageBodyActionCreator(body))
+            }
+
+            return <Dialogs
+                updateNewMessageBody={onNewMessageChange}
+                sendMessage={onSendMessageClick}
+                dialogsPage={state}
+            />
+        }}
+    </StoreContext.Consumer>
+
 }
 
--- social_network_2.0_js/src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ social_network_2.0_js/src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -5,26 +5,28 @@ import {StoreContext} from "../../StoreContext";
 
 export const MyPostsContainer = (props) => {
 
-    let state = props.store.getState()
-
-    let addPost = () => {
-        props.store.dispatch(addPostActionCreator())
-    }
-
-    let onPostChange = (text) => {
-        let action = updateNewPostActionCreator(text)
-        props.store.dispatch(action)
-    }
 
     return (
-        <StoreContext.Consumer>
-            (store) => (
-            <MyPosts
-                updateNewPostText={onPostChange}
-                addPost={addPost}
-                posts={state.profilePage.posts}
-                newPostText={state.profilePage.newPostText}
-            />)
+        <StoreContext.Consumer>{
+            (store) => {
+                let state = props.store.getState()
+
+                let addPost = () => {
+                    props.store.dispatch(addPostActionCreator())
+                }
+
+                let onPostChange = (text) => {
+                    let action = updateNewPostActionCreator(text)
+                    props.store.dispatch(action)
+                }
+                return <MyPosts
+                    updateNewPostText={onPostChange}
+                    addPost={addPost}
+                    posts={state.profilePage.posts}
+                    newPostText={state.profilePage.newPostText}
+                />
+            }}
         </StoreContext.Consumer>
+
     )
 }
