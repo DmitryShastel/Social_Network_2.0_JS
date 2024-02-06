00c3002c0005e637a917c307cd3eeada565f6e87
unknown <dmshastel@gmail.com>
2024-02-06 10:18:29 +0300

    The social-network 1-(50)51 is done

--- social_network_2.0_js/src/components/Users/Users.jsx
+++ social_network_2.0_js/src/components/Users/Users.jsx
@@ -1,37 +1,24 @@
 import React from "react";
 import s from './Users.module.css'
+import axios from "axios";
+import userPhoto from '../../assets/image/userPhoto.png'
 
 export let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://2.bp.blogspot.com/-w1KcztzMZTQ/VnXQHtY39GI/AAAAAAAAYQM/iB_PhFgDW9Y/s1600/bane.jpg',
-                    followed: true,
-                    fullName: 'Dima',
-                    status: 'I am the boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://image.stern.de/31693958/t/O6/v6/w1440/r1.7778/-/star-wars-schurke-was-wurde-aus-darth-maul.jpg',
-                    followed: false,
-                    fullName: 'Vasa',
-                    status: 'I am the boss too',
-                    location: {city: 'Moscow', country: 'Russia'}
-                }
-            ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(res => {
+                props.setUsers(res.data.items)
+            })
     }
-
-
     return (
-        <div>
+        <div className={s.wrapper}>
             {
                 props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img src={u.photoUrl} className={s.photo} className={s.photo}/>
+                        <img src={u.photos.small !== null
+                            ? u.photos.small
+                            : userPhoto} className={s.photo} className={s.photo}/>
                     </div>
                     <div>
                  {
@@ -46,15 +33,36 @@ export let Users = (props) => {
                 </span>
                     <span>
                     <span>
-                         <div>{u.fullName}</div>
+                         <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                 </span>
                 </div>)}
         </div>
     )
 }
+
+
+//old user data
+// [
+//     {
+//         id: 1,
+//         photoUrl: 'https://2.bp.blogspot.com/-w1KcztzMZTQ/VnXQHtY39GI/AAAAAAAAYQM/iB_PhFgDW9Y/s1600/bane.jpg',
+//         followed: true,
+//         fullName: 'Dima',
+//         status: 'I am the boss',
+//         location: {city: 'Minsk', country: 'Belarus'}
+//     },
+//     {
+//         id: 2,
+//         photoUrl: 'https://image.stern.de/31693958/t/O6/v6/w1440/r1.7778/-/star-wars-schurke-was-wurde-aus-darth-maul.jpg',
+//         followed: false,
+//         fullName: 'Vasa',
+//         status: 'I am the boss too',
+//         location: {city: 'Moscow', country: 'Russia'}
+//     }
+// ]
