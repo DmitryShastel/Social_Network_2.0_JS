f5f7899a2c1a78d3fc828333e7c9e29b3d6541a5
unknown <dmshastel@gmail.com>
2024-02-06 12:00:57 +0300

    The social-network 1-53 is done

--- social_network_2.0_js/src/components/Users/Users.jsx
+++ social_network_2.0_js/src/components/Users/Users.jsx
@@ -3,21 +3,22 @@ import s from './Users.module.css'
 import axios from "axios";
 import userPhoto from '../../assets/image/userPhoto.png'
 
-export let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(res => {
-                    props.setUsers(res.data.items)
-                })
-        }
-    }
-    return (
 
-        <div className={s.wrapper}>
-            <button onClick={getUsers}>Get Users</button>
-            {
-                props.users.map(u => <div key={u.id}>
+export class Users extends React.Component {
+
+    constructor(props) {
+        super(props)
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(res => {
+                props.setUsers(res.data.items)
+            })
+    }
+
+    render() {
+        return (
+            <div className={s.wrapper}>
+                {
+                    this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small !== null
@@ -28,14 +29,14 @@ export let Users = (props) => {
                  {
                      u.followed
                          ? <button onClick={() => {
-                             props.unfollow(u.id)
+                             this.props.unfollow(u.id)
                          }}>Unfollow</button>
                          : <button onClick={() => {
-                             props.follow(u.id)
+                             this.props.follow(u.id)
                          }}>Follow</button>}
                     </div>
                 </span>
-                    <span>
+                        <span>
                     <span>
                          <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -45,10 +46,59 @@ export let Users = (props) => {
                         <div>{'u.location.city'}</div>
                     </span>
                 </span>
-                </div>)}
-        </div>
-    )
+                    </div>)}
+            </div>
+        )
+    }
 }
+
+
+// export let Users = (props) => {
+//     let getUsers = () => {
+//         if (props.users.length === 0) {
+//             axios.get('https://social-network.samuraijs.com/api/1.0/users')
+//                 .then(res => {
+//                     props.setUsers(res.data.items)
+//                 })
+//         }
+//     }
+//     return (
+//
+//         <div className={s.wrapper}>
+//             <button onClick={getUsers}>Get Users</button>
+//             {
+//                 props.users.map(u => <div key={u.id}>
+//                 <span>
+//                     <div>
+//                         <img src={u.photos.small !== null
+//                             ? u.photos.small
+//                             : userPhoto} className={s.photo} className={s.photo}/>
+//                     </div>
+//                     <div>
+//                  {
+//                      u.followed
+//                          ? <button onClick={() => {
+//                              props.unfollow(u.id)
+//                          }}>Unfollow</button>
+//                          : <button onClick={() => {
+//                              props.follow(u.id)
+//                          }}>Follow</button>}
+//                     </div>
+//                 </span>
+//                     <span>
+//                     <span>
+//                          <div>{u.name}</div>
+//                         <div>{u.status}</div>
+//                     </span>
+//                     <span>
+//                         <div>{'u.location.country'}</div>
+//                         <div>{'u.location.city'}</div>
+//                     </span>
+//                 </span>
+//                 </div>)}
+//         </div>
+//     )
+// }
 
 
 //old user data
