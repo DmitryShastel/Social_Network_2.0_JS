835f705a224121d96b1a7cfdaa50d5e4de101052
unknown <dmshastel@gmail.com>
2024-02-08 01:03:46 +0300

    The social-network 1-54 is done

--- social_network_2.0_js/src/components/Users/Users.jsx
+++ social_network_2.0_js/src/components/Users/Users.jsx
@@ -6,11 +6,10 @@ import userPhoto from '../../assets/image/userPhoto.png'
 
 export class Users extends React.Component {
 
-    constructor(props) {
-        super(props)
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res => {
-                props.setUsers(res.data.items)
+                this.props.setUsers(res.data.items)
             })
     }
 
@@ -51,72 +50,3 @@ export class Users extends React.Component {
         )
     }
 }
-
-
-// export let Users = (props) => {
-//     let getUsers = () => {
-//         if (props.users.length === 0) {
-//             axios.get('https://social-network.samuraijs.com/api/1.0/users')
-//                 .then(res => {
-//                     props.setUsers(res.data.items)
-//                 })
-//         }
-//     }
-//     return (
-//
-//         <div className={s.wrapper}>
-//             <button onClick={getUsers}>Get Users</button>
-//             {
-//                 props.users.map(u => <div key={u.id}>
-//                 <span>
-//                     <div>
-//                         <img src={u.photos.small !== null
-//                             ? u.photos.small
-//                             : userPhoto} className={s.photo} className={s.photo}/>
-//                     </div>
-//                     <div>
-//                  {
-//                      u.followed
-//                          ? <button onClick={() => {
-//                              props.unfollow(u.id)
-//                          }}>Unfollow</button>
-//                          : <button onClick={() => {
-//                              props.follow(u.id)
-//                          }}>Follow</button>}
-//                     </div>
-//                 </span>
-//                     <span>
-//                     <span>
-//                          <div>{u.name}</div>
-//                         <div>{u.status}</div>
-//                     </span>
-//                     <span>
-//                         <div>{'u.location.country'}</div>
-//                         <div>{'u.location.city'}</div>
-//                     </span>
-//                 </span>
-//                 </div>)}
-//         </div>
-//     )
-// }
-
-
-//old user data
-// [
-//     {
-//         id: 1,
-//         photoUrl: 'https://2.bp.blogspot.com/-w1KcztzMZTQ/VnXQHtY39GI/AAAAAAAAYQM/iB_PhFgDW9Y/s1600/bane.jpg',
-//         followed: true,
-//         fullName: 'Dima',
-//         status: 'I am the boss',
-//         location: {city: 'Minsk', country: 'Belarus'}
-//     },
-//     {
-//         id: 2,
-//         photoUrl: 'https://image.stern.de/31693958/t/O6/v6/w1440/r1.7778/-/star-wars-schurke-was-wurde-aus-darth-maul.jpg',
-//         followed: false,
-//         fullName: 'Vasa',
-//         status: 'I am the boss too',
-//         location: {city: 'Moscow', country: 'Russia'}
-//     }
-// ]
